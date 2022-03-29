2d939e1825aa0edde70036e410404dd930b6b8c3
maliksalem1 <99354681+maliksalem1@users.noreply.github.com>
2022-03-29 17:10:23 +0000

    js, index

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Created by: maliksalem1
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const Abase = parseInt(document.getElementById('Abase').value)
+  const Bbase = parseInt(document.getElementById('Bbase').value)
+const Hight = parseInt(document.getElementById('Hight').value)
+
+  // process 
+  const area = [(a + b) 1 / 2 x h]
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' mm²'
+}
